eb756eda2469436ba517661d9471aa691ccc387a
Chris Zuber <shgysk8zer0@gmail.com>
2018-08-05 17:52:41 -0700

    Add PWA resouces & scripts

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import './std-js/shims.js';
 import './std-js/deprefixer.js';
-import {$, ready} from './std-js/functions.js';
+import {$, ready, registerServiceWorker} from './std-js/functions.js';
 import FeedList from './components/feed-list.js';
 import FeedForm from './components/feed-form.js';
 import CopyrightElement from './components/copyright-element.js';
@@ -10,8 +10,11 @@ customElements.define('copyright-element', CopyrightElement);
 customElements.define('feed-form', FeedForm, {extends: 'form'});
 
 ready().then(async () => {
-	const $doc = $(document.documentElement);
-	$doc.replaceClass('no-js', 'js');
+	document.documentElement.classList.replace('no-js', 'js');
+
+	if (document.documentElement.dataset.hasOwnProperty('serviceWorker')) {
+		registerServiceWorker(document.documentElement.dataset.serviceWorker);
+	}
 
 	$('[data-show-modal]').click(event => $(event.target.dataset.showModal).showModal());
 	$('[data-close]').click(event => $(event.target.dataset.close).close());
